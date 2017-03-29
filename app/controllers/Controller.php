303c8b635f05d6b63196f4b2b4ede2a46429b3f9
Pablo Cleij <pablocley@hotmail.com>
2017-03-29 12:09:07 +0200

    Removed and added comments

--- app/controllers/Controller.php
+++ app/controllers/Controller.php
@@ -4,7 +4,9 @@ namespace App\Controllers;
 
 class Controller {
 
+  // Container vanuit de app
   protected $container;
+  // Masterkey
   protected $masterkey = "YOUR_SECRET_CODE";
 
   public function __construct($container){
@@ -12,7 +14,9 @@ class Controller {
   }
 
   public function __get($property){
+    // Als de property bestaat
     if($this->container->{$property}){
+      // Return de property
       return $this->container->{$property};
     }
   }
@@ -20,49 +24,48 @@ class Controller {
   public function index($request, $response){
     // Sample data
     $data = array('name' => 'Pablo', 'age' => 22);
+    // Response in JSON
     $newResponse = $response->withJson($data);
+    // Return de response
     return $newResponse;
-    //return $this->view->render($response, 'index.html', [
-    //    'name' => '',
-    //]);
   }
 
   public function hello($request, $response, $args){
-    // Sample data
+    // Sample data vanuit url
     $data = array('intent' => 'Hello', 'to' => $args['name']);
+    // Response in JSON
     $newResponse = $response->withJson($data);
+    // Return de response
     return $newResponse;
-    //return $this->view->render($response, 'index.html', [
-    //    'name' => '',
-    //]);
   }
 
   public function action($request, $response, $args){
-    // Sample data
+    // Sample data vanuit url
     $data = array('intent' => $args['intent'], 'who' => $args['uid']);
+    // Response in JSON
     $newResponse = $response->withJson($data);
+    // Return de response
     return $newResponse;
-    //return $this->view->render($response, 'index.html', [
-    //    'name' => '',
-    //]);
   }
 
   public function protect($request, $response, $args){
-    // Sample data
+    // Check voor meegegeven key
     if ($args['key'] == $this->masterkey){
+      // Data als key zelfde als masterkey is
       $data = array('success' => true, 'connected' => true, 'errors' => false);
+      // Response in JSON
       $newResponse = $response->withJson($data);
+      // Return de response
       return $newResponse;
     } else {
+      // Data als key niet zelfde als masterkey is
       $data = array('success' => false, 'connected' => false, 'errors' => ['key' => 'Wrong master key']);
+      // Response in JSON
       $newResponse = $response->withJson($data);
+      // Return de response
       return $newResponse;
     }
 
-
-    //return $this->view->render($response, 'index.html', [
-    //    'name' => '',
-    //]);
   }
 
 }
