e4824004fe7cfb6318ca8587b8fa0fbfb3311240
Pablo Cleij <pablocley@hotmail.com>
2017-04-18 13:14:45 +0200

    Added comments throughout the controller

--- app/controllers/Controller.php
+++ app/controllers/Controller.php
@@ -23,7 +23,6 @@ class Controller {
   public function index($request, $response){
     // Sample data vanuit url
     $data = array('status' => 'server up and running!');
-
     // Response in JSON
     $newResponse = $response->withJson($data);
     // Return de response
@@ -31,11 +30,18 @@ class Controller {
   }
 
   public function client($request, $response, $args) {
-    $resp = $this->senz2->getToken();
+    // Get id from url param
     $clientid = $args['id'];
+
+    // Get token through Senz2
+    $resp = $this->senz2->getToken();
+
+    // Use token to fetch client data with client id
     $resp2 = $this->senz2->getClientData($clientid, $resp->access_token);
+
     // Response in JSON
     $newResponse = $response->withJson($resp2);
+
     // Return de response
     return $newResponse;
   }
@@ -61,24 +67,41 @@ class Controller {
 
   public function auth($request, $response, $args){
     // Query gebruiker_id vanuit url
+    // Als request een post request is
     if ($request->isPost()){
+      // Post data
       $parsedBody = $request->getParsedBody();
+
+      // Username en password vanuit post data
       $username = strtolower($parsedBody['username']);
       $password = $parsedBody['password'];
 
+      // Kijk of combinatie van username en password bestaat in de database
       $arr = $this->db->select("SELECT * FROM gebruiker WHERE username = :username AND password = :password", array(':username' => $username, ':password' => $password), true);
+      // Als array niet leeg is
       if(!empty($arr)){
+        // Set up data object met de benodigde informatie
         $data = array("status" => "Success", "user" => array('id' => $arr['id'], 'token' => $arr['token'], 'username' => $arr['username'], 'name' => $arr['name'], 'surname' => $arr['surname']));
+        // Maak json van data arraay
         $newResponse = $response->withJson($data);
+        // Return response(JSON)
         return $newResponse;
       } else {
+        // Set up data object met de benodige informatie
         $data = array("error" => "404", "status" => "No users found for that credentials");
+        // Maak json van data array
         $newResponse = $response->withJson($data);
+        // Return response(JSON)
         return $newResponse;
       }
-    } else {
+    }
+    // Als request niet post request is
+    else {
+      // Set up data object
       $data = array("error" => "Not a post method");
+      // Create JSON from data object
       $newResponse = $response->withJson($data);
+      // Return response (JSON)
       return $newResponse;
     }
 
@@ -87,7 +110,10 @@ class Controller {
   public function register($request, $response, $args){
     // Is request a post method?
     if ($request->isPost()){
+      // Krijgt post data vanuit de body
       $parsedBody = $request->getParsedBody();
+
+      // Pakt benodigde informatie (Activatiecode, Username, Password, Naam (optioneel), Surname (optioneel), Email (optioneel) )
       $activation = $parsedBody['activation'];
       $username = strtolower($parsedBody['username']);
       $password = $parsedBody['password'];
@@ -98,18 +124,28 @@ class Controller {
       // Check if activation code is in table
       $arr = $this->db->select("SELECT * FROM codes WHERE code = :code", array(':code' => $activation));
 
+      // Als array niet leeg is
       if(!empty($arr)){
-
         // Create token for user
         $arr2 = $this->db->select("SELECT * FROM token WHERE used = 0 LIMIT 1", array(), true);
+        // Token in variabel
         $token = $arr2['token'];
+
+        // Update de token table dat de token in use is (UPDATE functie returned hoeveel rows aangepast zijn)
         $affectedrows = $this->db->update('token', array('used' => 1), array('token' => $token));
+
+        // Als er geen affected rows zijn
         if (!$affectedrows){
+          // Maak data object
           $data = array("error" => "404", "status" => "Token not updated correctly");
+          // Maak json van data object
           $newResponse = $response->withJson($data);
+          // Return de response (JSON)
           return $newResponse;
         }
+        // Insert nieuwe token voor andere nieuwe gebruikers
         $this->db->insert('token', array('token' => md5(uniqid(rand(), true))));
+
         // Prepare user with details to register
         $user = array("username" => $username, "password" => $password, "name" => $name, "surname" => $surname, "email" => $email, "token" => $token);
 
@@ -118,25 +154,39 @@ class Controller {
 
         // If returned id
         if($id){
+          // Creer data array
           $data = array("status" => "Success");
+          // Creer JSON vanuit data array
           $newResponse = $response->withJson($data);
+          // Returned response (JSON)
           return $newResponse;
         }
         // Inserting went wrong
         else {
+          // Set up data array
           $data = array("error" => "404", "status" => "Inserting went wrong");
+          // Creer JSON vanuit data array
           $newResponse = $response->withJson($data);
+          // Returned the response (JSON)
           return $newResponse;
         }
       } else {
         // Return false because no valid activation code
+        // Set up data array
         $data = array("error" => "404", "status" => "Not a valid activation code");
+        // Convert data array into JSON
         $newResponse = $response->withJson($data);
+        // Return response (JSON)
         return $newResponse;
       }
-    } else {
+    }
+    // Als geen post request is
+    else {
+      // set up data array
       $data = array("error" => "Not a post method");
+      // Convert data array into JSON
       $newResponse = $response->withJson($data);
+      // Return response (JSON)
       return $newResponse;
     }
 
